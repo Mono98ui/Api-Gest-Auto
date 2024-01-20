d49213bed824573e7295f15568a7995ef75f148d
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-01-20 13:00:55 -0500

    API working

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+var cors = require('cors')
+
+const MONGO_DB = process.env.MONGO_URL
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res)=>{
+  res.send('Hello There')
+})
+
+
+// Start the server
+const server = app.listen(PORT, async () => {
+  console.log(`Example app listening on port ${PORT}`)
+  await mongoose.connect(MONGO_DB).then(()=>{
+  console.log("Connectedto DB sucessfully")
+  }).catch((error)=>{
+  console.log(error)
+  })
+});
+
+module.exports = server
